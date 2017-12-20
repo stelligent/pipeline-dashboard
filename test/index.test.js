bc4e532bb7535225176b643be6b4a5885f8eb62f
Casey Lee <casey.lee@stelligent.com>
2017-12-20 09:27:54 -0800

    update template to include policies rather than IAM roles

--- test/index.test.js
+++ test/index.test.js
@@ -13,6 +13,120 @@ var sinon = require('sinon');
 describe( 'handlePipelineEvent', function() {
     [
         {
+            description: "pipeline multiple success",
+            pipelineHistory: {
+                'pipelineExecutionSummaries': [
+                    {
+                        'pipelineExecutionId': 'xxx',
+                        'status': 'Succeeded',
+                        'startTime': new Date(1492830907000),
+                        'lastUpdateTime': new Date(1492831007000),
+                    },
+                    {
+                        'pipelineExecutionId': 'aaa',
+                        'status': 'Succeeded',
+                        'startTime': new Date(1492830207000),
+                        'lastUpdateTime': new Date(1492830407000),
+                    }
+                ]
+            },
+            event: {
+                'time': '2017-04-22T03:16:47Z',
+                'detail-type': 'CodePipeline Pipeline Execution State Change',
+                'detail': {
+                    'pipeline': 'my-pipeline',
+                    'execution-id': 'xxx',
+                    'state': 'SUCCEEDED',
+                }
+            },
+            metrics: [{
+                "Namespace": "Pipeline",
+                "MetricData": [
+                    {
+                        "Timestamp": new Date(1492831007000),
+                        "MetricName": "SuccessCount",
+                        "Unit": "Count",
+                        "Dimensions": [
+                            {Name: "PipelineName", Value: "my-pipeline"},
+                        ],
+                        "Value": 1,
+                    },
+                    {
+                        "Timestamp": new Date(1492831007000),
+                        "MetricName": "SuccessCycleTime",
+                        "Unit": "Seconds",
+                        "Dimensions": [
+                            {Name: "PipelineName", Value: "my-pipeline"},
+                        ],
+                        "Value": 600,
+                    },
+                    {
+                        "Timestamp": new Date(1492831007000),
+                        "MetricName": "SuccessLeadTime",
+                        "Unit": "Seconds",
+                        "Dimensions": [
+                            {Name: "PipelineName", Value: "my-pipeline"},
+                        ],
+                        "Value": 100,
+                    },
+                    {
+                        "Timestamp": new Date(1492831007000),
+                        "MetricName": "DeliveryLeadTime",
+                        "Unit": "Seconds",
+                        "Dimensions": [
+                            {Name: "PipelineName", Value: "my-pipeline"},
+                        ],
+                        "Value": 100,
+                    }
+                ]
+            }]
+        },
+        {
+            description: "pipeline succeeded",
+            pipelineHistory: {
+                'pipelineExecutionSummaries': [
+                    {
+                        'pipelineExecutionId': 'xxx',
+                        'status': 'Succeeded',
+                        'startTime': new Date(1492830707000),
+                        'lastUpdateTime': new Date(1492830907000),
+                    }
+                ]
+            },
+            event: {
+                'time': '2017-04-22T03:31:47Z',
+                'detail-type': 'CodePipeline Pipeline Execution State Change',
+                'detail': {
+                    'pipeline': 'my-pipeline',
+                    'execution-id': 'xxx',
+                    'state': 'SUCCEEDED',
+                }
+            },
+            metrics: [{
+                "Namespace": "Pipeline",
+                "MetricData": [
+                    {
+                        "Timestamp": new Date(1492831907000),
+                        "MetricName": "SuccessCount",
+                        "Unit": "Count",
+                        "Dimensions": [
+                            {Name: "PipelineName", Value: "my-pipeline"},
+                        ],
+                        "Value": 1,
+                    },
+                    {
+                        "Timestamp": new Date(1492831907000),
+                        "MetricName": "SuccessLeadTime",
+                        "Unit": "Seconds",
+                        "Dimensions": [
+                            {Name: "PipelineName", Value: "my-pipeline"},
+                        ],
+                        "Value": 200,
+                    }
+                ]
+            }]
+        },
+        {
             description: "action succeeded",
             event: {
                 'time': '2017-04-22T03:31:47Z',
@@ -64,41 +178,6 @@ describe( 'handlePipelineEvent', function() {
                         "Dimensions": [
                             {Name: "PipelineName", Value: "my-pipeline"},
                             {Name: "StageName", Value: "commit"},
-                        ],
-                        "Value": 1,
-                    }
-                ]
-            }]
-        },
-        {
-            description: "pipeline succeeded",
-            pipelineHistory: {
-                'pipelineExecutionSummaries': [
-                    {
-                        'pipelineExecutionId': 'xxx',
-                        'startTime': new Date(1492830707000),
-                        'lastUpdateTime': new Date(1492830907000),
-                    }
-                ]
-            },
-            event: {
-                'time': '2017-04-22T03:31:47Z',
-                'detail-type': 'CodePipeline Pipeline Execution State Change',
-                'detail': {
-                    'pipeline': 'my-pipeline',
-                    'execution-id': 'xxx',
-                    'state': 'SUCCEEDED',
-                }
-            },
-            metrics: [{
-                "Namespace": "Pipeline",
-                "MetricData": [
-                    {
-                        "Timestamp": new Date(1492831907000),
-                        "MetricName": "SuccessCount",
-                        "Unit": "Count",
-                        "Dimensions": [
-                            {Name: "PipelineName", Value: "my-pipeline"},
                         ],
                         "Value": 1,
                     }
@@ -423,6 +502,62 @@ describe( 'handlePipelineEvent', function() {
                 ]
             }]
         },
+        {
+            description: "pipeline state unchanged",
+            pipelineHistory: {
+                'pipelineExecutionSummaries': [
+                    {
+                        'pipelineExecutionId': 'xxx',
+                        'status': 'Failed',
+                        'startTime': new Date(1492830907000),
+                        'lastUpdateTime': new Date(1492831007000),
+                    },
+                    {
+                        'pipelineExecutionId': 'yyy',
+                        'status': 'Failed',
+                        'startTime': new Date(1492830807000),
+                    },
+                    {
+                        'pipelineExecutionId': 'zzz',
+                        'status': 'Succeeded',
+                        'startTime': new Date(1492830507000),
+                        'lastUpdateTime': new Date(1492830607000),
+                    }
+                ]
+            },
+            event: {
+                'time': '2017-04-22T03:16:47Z',
+                'detail-type': 'CodePipeline Pipeline Execution State Change',
+                'detail': {
+                    'pipeline': 'my-pipeline',
+                    'execution-id': 'xxx',
+                    'state': 'FAILED',
+                }
+            },
+            metrics: [{
+                "Namespace": "Pipeline",
+                "MetricData": [
+                    {
+                        "Timestamp": new Date(1492831007000),
+                        "MetricName": "FailureCount",
+                        "Unit": "Count",
+                        "Dimensions": [
+                            {Name: "PipelineName", Value: "my-pipeline"},
+                        ],
+                        "Value": 1,
+                    },
+                    {
+                        "Timestamp": new Date(1492831007000),
+                        "MetricName": "FailureLeadTime",
+                        "Unit": "Seconds",
+                        "Dimensions": [
+                            {Name: "PipelineName", Value: "my-pipeline"},
+                        ],
+                        "Value": 100,
+                    }
+                ]
+            }]
+        },
     ].forEach(scenario => {
         if (scenario.skip) {
             return;
@@ -446,13 +581,14 @@ describe( 'handlePipelineEvent', function() {
             return LambdaTester(index.handlePipelineEvent)
                 .event(scenario.event)
                 .expectResult((result, additional) => {
+                    AWS.restore('CloudWatch');
+                    AWS.restore('CodePipeline');
+
                     expect(putMetricDataSpy).to.have.callCount(scenario.metrics.length);
                     scenario.metrics.forEach(metric => {
                         expect(putMetricDataSpy).to.have.been.calledWith(metric);
                     });
 
-                    AWS.restore('CloudWatch');
-                    AWS.restore('CodePipeline');
                 });
         });
     });
